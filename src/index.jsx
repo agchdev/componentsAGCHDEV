b9153d58ce5d215b9d4bb2689fb32921bfd6dc91
agchdev <alex.chinchi02@gmail.com>
2025-11-28 18:46:43 +0100

    new index

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NavModern } from './components/NavModern/NavModern'
 import { ButtonModern } from './components/ButtonModern/ButtonModern'
 import { HeaderModern } from './components/HeaderModern/HeaderModern'
@@ -6,9 +5,3 @@ import { FooterModern } from './components/FooterModern/FooterModern'
 
 export { NavModern, ButtonModern, HeaderModern, FooterModern }
 export default { NavModern, ButtonModern, HeaderModern, FooterModern }
-=======
-import { NavModern } from './NavModern'
-
-export { NavModern }
-export default { NavModern }
->>>>>>> main
